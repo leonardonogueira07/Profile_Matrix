a23337ab9296cf895ad1d62363a44c33efaba216
Leonardo Nogueira <leojunio007@gmail.com>
2021-11-02 00:27:35 -0300

    first commit

---
+++ Passport/assets/_js/script.js
@@ -0,0 +1,64 @@
+
+//Links alert
+const socialMedia = {
+    gitHub : 'master-jr',
+    youtube : 'UC_BSzKucblA4G8G0eAjCJ4A',
+    facebook : '100015134868886',
+    instagram : '@nogueiramaster'
+}
+/* console.log(socialMedia) */
+function changeNameMediaSocial() {
+
+    for(let li of socialLink.children) {
+       const social = li.getAttribute('class')
+      /*  alert(li.children.href) */
+
+      /* li.children[0].href = 'https://www.google.com' */
+       li.children[0].href = `https://${social}.com/$
+       {socialMedia[social]}`
+
+       /* alert(social) */
+       /* alert(li.children[0].href) */
+    }    
+}
+changeNameMediaSocial();
+
+
+
+//const Matrix
+const theMatrix = document.getElementById('matrix');
+const ctx = theMatrix.getContext('2d');
+const w = theMatrix.width = document.body.offsetWidth;
+const h = theMatrix.height = document.body.offsetHeight;
+const cols = Math.floor(w/20) + 1;
+const yPos = Array(cols).fill(0);
+
+//ctx Matrix
+ctx.fillStyle = '#000';
+ctx.fillRect(0,0,w,h);
+
+//Function Matrix
+function AMatrix() {
+    ctx.fillStyle = '#0001';
+    ctx.fillRect(0,0,w,h);
+
+    ctx.fillStyle = '#0f0';
+    ctx.font = '15pt monospace';
+
+    yPos.forEach((y,ind) => {
+        const text = String.fromCharCode(Math.random() *128);
+        const x = ind * 20;
+        ctx.fillText(text, x, y);
+
+        if(y > 100 + Math.random() * 10000) yPos[ind] = 0;
+        else yPos[ind] = y + 20;
+    });
+}
+setInterval(AMatrix, 50);
+/*
+    camelCase
+    PascalCase
+    snake_case
+    kebab-case
+
+*/
